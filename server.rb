044ed309069fdef60ac4cc4527a53b2051b4e039
rachana-prajapati <rachanaprajapati101@gmail.com>
2018-04-01 23:31:13 +0530

    Update server.rb

--- server.rb
+++ server.rb
@@ -1,7 +1,22 @@
 require 'sinatra'
 require 'json'
+require 'net/http'
+require 'uri'
+
+set :port, 9000
 
 post '/payload' do
-  push = JSON.parse(request.body.read)
-  puts "I got some JSON: #{push.inspect}"
+	push = JSON.parse(request.body.read)
+	puts "I got some JSON: #{push.inspect}"
+	uri = URI.parse("https://hooks.slack.com/services/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")
+	header = {'Content-Type': 'text/json'}
+	# Create the HTTP objects
+	http = Net::HTTP.new(uri.host, uri.port)
+	request = Net::HTTP::Post.new(uri.request_uri, header)
+	request.body =  JSON.generate({"text":"hiii"})
+	
+	# Send the request
+	response = http.request(request)
+	#puts "response: "+response.to_str
+	puts "message sent to the bot.."
 end
